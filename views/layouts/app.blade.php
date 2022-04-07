4c81b38623c45d1900ecc839872928cf1182ae0e
namo1narayan <87004684+namo1narayan@users.noreply.github.com>
2022-04-07 21:59:37 +0530

    Add files via upload

---
+++ views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- <title>Document</title> --}}
+    {{-- <title>{{ config('app.name') }}</title> --}}
+
+        <!-- Fonts -->
+        {{-- <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"> --}}
+        {{-- //Include bootstrap CSS CDN here --}}
+
+        <style>
+            body {
+                font-family: 'Nunito';
+            }
+        </style>
+</head>
+<body>
+    
+    @yield('content')
+
+</body>
+{{-- //Include bootstrap JS CDN here  --}}
+
+</body>
+</html>
+
+
