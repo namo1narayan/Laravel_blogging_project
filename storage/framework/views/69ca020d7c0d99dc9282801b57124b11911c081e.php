fb01e66871a3aad39452ff693c2a499a6929e584
namo1narayan <87004684+namo1narayan@users.noreply.github.com>
2022-04-07 21:55:25 +0530

    Add files via upload

---
+++ storage/framework/views/69ca020d7c0d99dc9282801b57124b11911c081e.php
@@ -0,0 +1,20 @@
+<?php $__env->startSection('content'); ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 pt-2">
+                 <div class="row">
+                    <div class="col-8">
+                        <h1 class="display-one">Our Blog!</h1>
+                        <p>Enjoy reading our posts. Click on a post to read!</p>
+                    </div>
+                    <div class="col-4">
+                        <p>Create new Post</p>
+                        <a href="/blog/create/post" class="btn btn-primary btn-sm">Add Post</a>
+                    </div>
+                </div>                
+                
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\NAMO_NARAYAN_11903026\resources\views/welcome.blade.php ENDPATH**/ ?>
